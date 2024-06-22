7e9f0cbfada9091b2719ce2039ab0aa776d542cb
Sourabh Bucha <sourabh.bucha@sysquare.com>
2024-06-22 14:34:11 +0530

    Cors setting

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ require('dotenv').config()
 const cors = require("cors");
 
 const generate = require("./routes/generate.route");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/chainxprests-llm/generate", generate);
+
 
 app.use(
   cors({
@@ -17,6 +15,12 @@ app.use(
   })
 );
 app.options("*", cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/chainxprests-llm/generate", generate);
+
+
 
 const PORT = process.env.PORT || 5000;
 
