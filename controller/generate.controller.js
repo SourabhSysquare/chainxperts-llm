41704ca346f8f156affdae99c828299e485f5141
Sourabh Bucha <sourabh.bucha@sysquare.com>
2024-06-22 14:25:55 +0530

    Added CORS

--- controller/generate.controller.js
+++ controller/generate.controller.js
@@ -1,7 +1,6 @@
 const generateService = require("../services/generate.service");
 
 const generatePromptResponse = async (req, res) => {
-    console.log(req.body.prompt)
     var response = await generateService.generatePrompt(req.body.prompt)
     res.status(200).json({
         message: response,
--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 require('dotenv').config()
+const cors = require("cors");
 
 const generate = require("./routes/generate.route");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/chainxprests-llm/generate", generate);
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
+app.options("*", cors());
 
 const PORT = process.env.PORT || 5000;
 
