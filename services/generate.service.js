914f9ec1c5c100bfeca658e96d6dc44303bd7385
Sourabh Bucha <sourabh.bucha@sysquare.com>
2024-06-23 00:09:25 +0530

    Save History

--- services/generate.service.js
+++ services/generate.service.js
@@ -12,10 +12,11 @@ endpoints.apiConfig.map((api) => {
     inputPrompt += `If the user wants his ${api.name} (${api.desc}) use the following endpoint : '${api.endpoints}'. \n`
 })
 
-
-inputDesc = "Provide the endpoint and api type which I can use for :" 
+inputDesc = "Provide the endpoint and api type which I can use for :"
 
 inputTopic = "Provide a suitable topic for the given question asked by user in 2-3 words, Topic: "
+
+
 const generatePrompt = async (prompt, bearerToken) => {
     const chatCompletion = await openai.chat.completions.create({
         messages: [{ role: 'system', content: inputPrompt }, { role: 'user', content: inputDesc + prompt }],
@@ -28,47 +29,45 @@ const generatePrompt = async (prompt, bearerToken) => {
     });
 
     response = chatCompletion.choices[0].message.content;
-    
+
     // Prase endpoint
     var endpointRequired = ""
-    
+
     endpoints.apiConfig.map((apiEnd) => {
-        if(response.includes(apiEnd.endpoints)) {
+        if (response.includes(apiEnd.endpoints)) {
             endpointRequired = apiEnd.endpoints;
         }
     })
 
+    //Save History  
+    const data = {
+        prompt: prompt,
+        title: topicCompletion.choices[0].message.content,
+        content: endpointRequired
+    };
+
+    const headers = {
+        Authorization: `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
+    };
+
+    await axios.put(process.env.SERVER_ENDPOINT + "/fin-manager/search-history", data, { headers })
+
     // if no api recieved
-    if(endpointRequired == ""){
-        return {topic: "No Related Data found", data: []}
+    if (endpointRequired == "") {
+        return { topic: "No Related Data found", data: [] }
     }
-    
+
     //Get API Response
-    
     const config = {
         headers: {
             Authorization: `Bearer ${bearerToken}`,
         },
     };
 
-    const res =  await axios.get(process.env.SERVER_ENDPOINT + endpointRequired, config);
-    
-    //Save History
-    await axios.get(process.env.SERVER_ENDPOINT + endpointRequired, config);
-    
-    const data = {
-        prompt: prompt,
-        title: topicCompletion.choices[0].message.content,
-        content: endpointRequired
-    };
-      
-    const headers = {
-        Authorization: `Bearer ${bearerToken}`,
-        'Content-Type': 'application/json'
-    };
-    
-    await axios.put(process.env.SERVER_ENDPOINT + "/fin-manager/search-history", data, { headers })
-    return {topic: topicCompletion.choices[0].message.content, data: res.data}
+    const res = await axios.get(process.env.SERVER_ENDPOINT + endpointRequired, config);
+
+    return { topic: topicCompletion.choices[0].message.content, data: res.data }
 }
 
 module.exports = {
